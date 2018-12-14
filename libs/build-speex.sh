740bd2996c00900c938c6f7c0fd41c85a84c1cea
KWANG HYOUN KIM <pisces@nhnent.com>
2018-12-15 01:57:20 +0900

    Recovery build-speex.sh

--- libs/build-speex.sh
+++ libs/build-speex.sh
@@ -15,191 +15,91 @@
 # limitations under the License.
 #
 
-# This script is based on projects below
-# https://github.com/cxjwin/speex_libs/blob/master/speex-1.2rc2/build_speex_ios.sh
+#----------
+# modify for your build tool
 
-#--------------------
-echo "===================="
-echo "[*] check host"
-echo "===================="
+FF_ALL_ARCHS_IOS6_SDK="armv7 armv7s i386"
+FF_ALL_ARCHS_IOS7_SDK="armv7 armv7s arm64 i386 x86_64"
+FF_ALL_ARCHS_IOS8_SDK="armv7 arm64 i386 x86_64"
+
+FF_ALL_ARCHS=$FF_ALL_ARCHS_IOS7_SDK
+
+#----------
+UNI_BUILD_ROOT=`pwd`
+LIBS="$UNI_BUILD_ROOT/build/universal"
+UNI_TMP="$UNI_BUILD_ROOT/tmp"
+UNI_TMP_LLVM_VER_FILE="$UNI_TMP/llvm.ver.txt"
+FF_TARGET=$1
 set -e
 
+#----------
+FF_LIBS="libspeex"
+OGG_LIBS="libogg"
 
-FF_XCRUN_DEVELOPER=`xcode-select -print-path`
-if [ ! -d "$FF_XCRUN_DEVELOPER" ]; then
-  echo "xcode path is not set correctly $FF_XCRUN_DEVELOPER does not exist (most likely because of xcode > 4.3)"
-  echo "run"
-  echo "sudo xcode-select -switch <xcode path>"
-  echo "for default installation:"
-  echo "sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer"
-  exit 1
-fi
-
-case $FF_XCRUN_DEVELOPER in
-     *\ * )
-           echo "Your Xcode path contains whitespaces, which is not supported."
-           exit 1
-          ;;
-esac
-
-
-#--------------------
-# include
-
-
-#--------------------
-# common defines
-FF_ARCH=$1
-if [ -z "$FF_ARCH" ]; then
-    echo "You must specific an architecture 'armv7, armv7s, arm64, i386, x86_64, ...'.\n"
-    exit 1
-fi
-
-
-FF_BUILD_ROOT=`pwd`
-FF_TAGET_OS="darwin"
-
-
-# speex build params
-export COMMON_FF_CFG_FLAGS=
-
-SPEEX_CFG_FLAGS=
-SPEEX_EXTRA_CFLAGS=
-SPEEX_CFG_CPU=
-
-# i386, x86_64
-SPEEX_CFG_FLAGS_SIMULATOR="--host=arm"
-
-# armv7, armv7s, arm64
-SPEEX_CFG_FLAGS_ARM=
-SPEEX_CFG_FLAGS_ARM="--host=arm-apple-darwin"
-
-echo "build_root: $FF_BUILD_ROOT"
-
-#--------------------
-echo "===================="
-echo "[*] config arch $FF_ARCH"
-echo "===================="
-
-FF_BUILD_NAME="unknown"
-FF_XCRUN_PLATFORM="iPhoneOS"
-FF_XCRUN_OSVERSION=
-FF_GASPP_EXPORT=
-
-if [ "$FF_ARCH" = "i386" ]; then
-    FF_BUILD_NAME="speex/i386"
-    FF_BUILD_NAME_OGG=ogg/i386
-    FF_XCRUN_PLATFORM="iPhoneSimulator"
-    FF_XCRUN_OSVERSION="-mios-simulator-version-min=6.0"
-    SPEEX_CFG_FLAGS="$SPEEX_CFG_FLAGS_SIMULATOR $SPEEX_CFG_FLAGS"
-elif [ "$FF_ARCH" = "x86_64" ]; then
-    FF_BUILD_NAME="speex/x86_64"
-    FF_BUILD_NAME_OGG=ogg/x86_64
-    FF_XCRUN_PLATFORM="iPhoneSimulator"
-    FF_XCRUN_OSVERSION="-mios-simulator-version-min=7.0"
-    SPEEX_CFG_FLAGS="$SPEEX_CFG_FLAGS_SIMULATOR $SPEEX_CFG_FLAGS"
-elif [ "$FF_ARCH" = "armv7" ]; then
-    FF_BUILD_NAME="speex/armv7"
-    FF_BUILD_NAME_OGG=ogg/armv7
-    FF_XCRUN_OSVERSION="-miphoneos-version-min=6.0"
-    SPEEX_CFG_FLAGS="$SPEEX_CFG_FLAGS_ARM $SPEEX_CFG_FLAGS"
-#    OPENSSL_CFG_CPU="--cpu=cortex-a8"
-elif [ "$FF_ARCH" = "armv7s" ]; then
-    FF_BUILD_NAME="speex/armv7s"
-    FF_BUILD_NAME_OGG=ogg/armv7s
-    SPEEX_CFG_CPU="--cpu=swift"
-    FF_XCRUN_OSVERSION="-miphoneos-version-min=6.0"
-    SPEEX_CFG_FLAGS="$SPEEX_CFG_FLAGS_ARM $SPEEX_CFG_FLAGS"
-elif [ "$FF_ARCH" = "arm64" ]; then
-    FF_BUILD_NAME="speex/arm64"
-    FF_BUILD_NAME_OGG=ogg/arm64
-    FF_XCRUN_OSVERSION="-miphoneos-version-min=7.0"
-    SPEEX_CFG_FLAGS="$SPEEX_CFG_FLAGS_ARM $SPEEX_CFG_FLAGS"
-    FF_GASPP_EXPORT="GASPP_FIX_XCODE5=1"
-else
-    echo "unknown architecture $FF_ARCH";
-    exit 1
-fi
-
-echo "build_name: $FF_BUILD_NAME"
-echo "platform:   $FF_XCRUN_PLATFORM"
-echo "osversion:  $FF_XCRUN_OSVERSION"
-
-#--------------------
-echo "===================="
-echo "[*] make ios toolchain $FF_BUILD_NAME"
-echo "===================="
-
-
-FF_BUILD_SOURCE="$FF_BUILD_ROOT/build/src/$FF_BUILD_NAME"
-FF_BUILD_PREFIX="$FF_BUILD_ROOT/build/thin/$FF_BUILD_NAME"
-
-mkdir -p $FF_BUILD_PREFIX
-
-
-FF_XCRUN_SDK=`echo $FF_XCRUN_PLATFORM | tr '[:upper:]' '[:lower:]'`
-FF_XCRUN_SDK_PLATFORM_PATH=`xcrun -sdk $FF_XCRUN_SDK --show-sdk-platform-path`
-FF_XCRUN_SDK_PATH=`xcrun -sdk $FF_XCRUN_SDK --show-sdk-path`
-FF_XCRUN_CC="xcrun -sdk $FF_XCRUN_SDK clang"
-
-export CROSS_TOP="$FF_XCRUN_SDK_PLATFORM_PATH/Developer"
-export CROSS_SDK=`echo ${FF_XCRUN_SDK_PATH/#$CROSS_TOP\/SDKs\//}`
-export BUILD_TOOL="$FF_XCRUN_DEVELOPER"
-export CC="$FF_XCRUN_CC -arch $FF_ARCH $FF_XCRUN_OSVERSION"
-
-echo "build_source: $FF_BUILD_SOURCE"
-echo "build_prefix: $FF_BUILD_PREFIX"
-echo "CROSS_TOP: $CROSS_TOP"
-echo "CROSS_SDK: $CROSS_SDK"
-echo "BUILD_TOOL: $BUILD_TOOL"
-echo "CC: $CC"
-
-#--------------------
-echo "\n--------------------"
-echo "[*] check OGG"
-echo "----------------------"
-
-CXXFLAGS="$CFLAGS"
-LDFLAGS="$CFLAGS"
-OGG=$FF_BUILD_ROOT/build/thin/$FF_BUILD_NAME_OGG
-
-if [ -f "${OGG}/lib/libogg.a" ]; then
-  SPEEX_CFG_FLAGS="$SPEEX_CFG_FLAGS --with-ogg=${OGG}"
-fi
-
-#--------------------
-echo "\n--------------------"
-echo "[*] configurate speex"
-echo "--------------------"
-
-SPEEX_CFG_FLAGS="$SPEEX_CFG_FLAGS --prefix=$FF_BUILD_PREFIX"
-
-# xcode configuration
-export DEBUG_INFORMATION_FORMAT=dwarf-with-dsym
-
-function configure() {
-  cd $FF_BUILD_SOURCE
-  if [ -f "./Makefile" ]; then
-      echo 'reuse configure'
-  elif [ -f "./configure" ]; then
-      echo 'already run autogen.sh'
-      echo "config: $SPEEX_CFG_FLAGS"
-      ./Configure \
-          $SPEEX_CFG_FLAGS
-      configure
-  else
-      echo 'should run autogen.sh first'
-      ./autogen.sh
-      configure
-  fi
+#----------
+echo_archs() {
+    echo "===================="
+    echo "[*] check xcode version"
+    echo "===================="
+    echo "FF_ALL_ARCHS = $FF_ALL_ARCHS"
 }
 
-configure
+do_lipo () {
+    LIB_FILE=$1
+    LIPO_FLAGS=
+    for ARCH in $FF_ALL_ARCHS
+    do
+        LIPO_FLAGS="$LIPO_FLAGS $UNI_BUILD_ROOT/build/thin/speex/$ARCH/lib/$LIB_FILE"
+    done
 
-#--------------------
-echo "\n--------------------"
-echo "[*] compile speex"
-echo "--------------------"
-set +e
-make
-make install
+    xcrun lipo -create $LIPO_FLAGS -output $LIBS/lib/$LIB_FILE
+    xcrun lipo -info $LIBS/lib/$LIB_FILE
+}
+
+do_lipo_all () {
+    mkdir -p $LIBS/lib
+    echo "lipo archs: $FF_ALL_ARCHS"
+    for FF_LIB in $FF_LIBS
+    do
+        do_lipo "$FF_LIB.a";
+    done
+
+    cp -R $UNI_BUILD_ROOT/build/thin/speex/armv7/include $LIBS
+}
+
+#----------
+if [ "$FF_TARGET" = "armv7" -o "$FF_TARGET" = "armv7s" -o "$FF_TARGET" = "arm64" ]; then
+    echo_archs
+    sh tools/do-compile-speex.sh $FF_TARGET
+elif [ "$FF_TARGET" = "i386" -o "$FF_TARGET" = "x86_64" ]; then
+    echo_archs
+    sh tools/do-compile-speex.sh $FF_TARGET
+elif [ "$FF_TARGET" = "lipo" ]; then
+    echo_archs
+    do_lipo_all
+elif [ "$FF_TARGET" = "all" ]; then
+    echo_archs
+    for ARCH in $FF_ALL_ARCHS
+    do
+        sh tools/do-compile-speex.sh $ARCH
+    done
+
+    do_lipo_all
+elif [ "$FF_TARGET" = "check" ]; then
+    echo_archs
+elif [ "$FF_TARGET" = "clean" ]; then
+    echo_archs
+    for ARCH in $FF_ALL_ARCHS
+    do
+        cd speex-$ARCH && git clean -xdf && cd -
+    done
+else
+    echo "Usage:"
+    echo "  compile-speex.sh armv7|arm64|i386|x86_64"
+    echo "  compile-speex.sh armv7s (obselete)"
+    echo "  compile-speex.sh lipo"
+    echo "  compile-speex.sh all"
+    echo "  compile-speex.sh clean"
+    echo "  compile-speex.sh check"
+    exit 1
+fi
